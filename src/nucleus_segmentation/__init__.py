0cedd6c6686b92c25eef29994616943114603541
Timothé Berland <timothe.berland@etu.emse.fr>
2026-09-21 18:39:49 +0200

    Update version file

--- src/nucleus_segmentation/__init__.py
+++ src/nucleus_segmentation/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+"""3D Nucleus Segmentation"""
+
+from .__version__ import __version__
+
+__all__ = [
+    '__version__',
+]
---
+++ src/nucleus_segmentation/__version__.py
@@ -0,0 +1,3 @@
+"""3D Nucleus Segmentation"""
+
+__version__ = '0.0.1'
