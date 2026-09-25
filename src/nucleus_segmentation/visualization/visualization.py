0cd2513f2641d93eb419c214c0c387e29232b7a3
Timothé Berland <timothe.berland@etu.emse.fr>
2026-09-26 00:22:54 +0200

    Add method to visualize the 3D data cubes

--- src/nucleus_segmentation/visualization/visualization.py
+++ src/nucleus_segmentation/visualization/visualization.py
@@ -1,6 +1,8 @@
+import gc
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import napari
 
 
 class Visualization:
@@ -21,3 +23,22 @@ class Visualization:
 
         plt.savefig(output_path, dpi=150)
         plt.close()
+
+    @staticmethod
+    def save_2d_view(data, output_path, camera_rotation):
+        output_path = Path(output_path)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+
+        viewer = napari.Viewer(show=True, ndisplay=3)
+        viewer.add_image(data, opacity=1.0, scale=(1, 1, 1))
+
+        viewer.scene.camera.angles = camera_rotation
+        viewer.scene.camera.zoom = 1
+        viewer.screenshot(path=output_path, canvas_only=True)
+
+        # Close
+        # viewer.close()
+
+        # Free memory
+        del viewer
+        gc.collect()
